3d62c35e4a48e9002a0684e1997dba2db5b275fd
Diego <diego.carcaustor@gmail.com>
2024-12-30 14:29:02 -0500

    hotfix/app-layout-store

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,14 +1,9 @@
 import { useDispatch, useSelector, useStore } from "react-redux";
-import type { AppDispatch, AppStore, RootState } from "./store";
 import type {
   UserDispatch,
   UserStore,
   RootUserState,
 } from "./features/user/userStore";
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
 
 export const useUserDispatch = useDispatch.withTypes<UserDispatch>();
 export const useUserSelector = useSelector.withTypes<RootUserState>();
